12c886a627a4dd6ddc6b1d461b7660baaeb0976b
heena <heena7sept@gmail.com>
2022-02-21 17:51:47 -0600

    Lily added a hobby

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hi My name is Heena, and I am in CS3443");
 		System.out.println("Hi My name is Loan, and I am in CS1083");
+		System.out.println("Hi My name is Lily, and I am in CS2073");
 		System.out.println("Hi My name is John, and I am in CS4363");
 
 	}
